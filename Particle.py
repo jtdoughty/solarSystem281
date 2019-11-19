ec6f2a6627f19ab030417d7d63fcae188ec69947
jtdoughty <57949285+jtdoughty@users.noreply.github.com>
2019-11-19 14:23:18 +0000

    Update Particle.py

--- Particle.py
+++ Particle.py
@@ -3,7 +3,7 @@ import numpy as np
 class Particle:
     """
     Class defining a massive particle with initial conditions, with an associated position and velocity updater.
-    ###############################
+    -------------------------------------
     Parameters:
     > Position: Initial position in space, as float array [x,y,z] (default [0,0,0] at origin)
     > Velocity: Initial velocity, as float array [x,y,z] (default [0,0,0] immobile)
@@ -11,23 +11,48 @@ class Particle:
     > Name: Name label of particle (default 'Ball')
     > Mass: Mass of particle (default 1.0) (unused)
     """
-    def __init__(self, Position=np.array([0,0,0], dtype=float), Velocity=np.array([0,0,0], dtype=float), Acceleration=np.array([0,-10,0], dtype=float), Name='Ball', Mass=1.0, G=6.67408E-11):
+    def __init__(self, Position=np.array([0,0,0], dtype=float), Velocity=np.array([0,0,0], dtype=float), Acceleration=np.array([0,-10,0], dtype=float), name='Ball', Mass=1.0, method=1):
         self.position=np.array(Position, dtype=float)
         self.velocity=np.array(Velocity, dtype=float)
         self.acceleration=np.array(Acceleration, dtype=float)
-        self.Name=Name
-        self.mass=Mass
-        self.G=G
+        self.name=str(name)
+        self.mass=float(Mass)
+        self.setMethod(method)
+
+    G=6.67408E-11
 
     def __repr__(self):
-        return 'Particle: {0}, Mass: {1:12.3e}, Position: {2}, Velocity: {3}, Acceleration: {4}'.format(self.Name,self.mass,self.position, self.velocity,self.acceleration)
+        return 'Particle: {0}, Mass: {1:12.3e}, Position: {2}, Velocity: {3}, Acceleration: {4}'.format(self.name,self.mass,self.position, self.velocity,self.acceleration)
 
-    def update(self, deltaT):
+    def setMethod(self, method):
+        if method == 1:
+            self.update = self.euler
+        elif method == 2:
+            self.update = self.eulerCromer
+        #elif method == 3:
+        #    self.update = self.crect
+        else:
+            raise ValueError(
+                "Unrecognised integration method. Method must be 1 "
+                "(trapezoid), 2 (left rectangle), or 3 (centre rectangle)."
+            )
+
+    def euler(self, deltaT):
         """
-        Updates position and velocity over a time increment, assuming constant acceleration
-        #############################
+        Updates position and velocity over a time increment, assuming constant acceleration, updating position using velocity before incrementing.
+        --------------------------------
         Parameters:
         > deltaT: Time increment (no default)
         """
         self.position += self.velocity*deltaT
         self.velocity += self.acceleration*deltaT
+
+    def eulerCromer(self, deltaT):
+        """
+        Updates position and velocity over a time increment, assuming constant acceleration, updating position using velocity after incrementing.
+        --------------------------------
+        Parameters:
+        > deltaT: Time increment (no default)
+        """
+        self.velocity += self.acceleration*deltaT
+        self.position += self.velocity*deltaT
