511e17e81a6e6b8a998d91aba56db77f524b3992
jtdoughty <57949285+jtdoughty@users.noreply.github.com>
2019-12-02 12:06:46 +0000

    Add files via upload

---
+++ runSimulation.py
@@ -0,0 +1,15 @@
+import numpy as np
+from Planets import planetList
+from Particle import Particle
+from solarSystem import GroupOfParticles
+
+listname = np.loadtxt('initialise.csv',dtype=str,delimiter=',',usecols=(0))
+Delta = np.loadtxt('initialise.csv',delimiter=',',usecols=(1))
+Iterations = np.loadtxt('initialise.csv',delimiter=',',usecols=(2))
+methodNo = np.loadtxt('initialise.csv',delimiter=',',usecols=(3))
+
+planetset = planetList(listname)
+listOfPlanets = planetset.makeList()
+Particle.setMethod(methodNo)
+SolarSystem = GroupOfParticles(Delta,listOfPlanets,Iterations)
+SolarSystem.plotGraph()
